d4fb0d3b7f26963738c323258ac0af973b46f84d
Nicole <hfx666666@gmail.com>
2024-02-18 15:02:42 +0800

    feat: design

--- src/components/WorkDisplay.tsx
+++ src/components/WorkDisplay.tsx
@@ -23,14 +23,21 @@ export default function WorkDisplay(props: Props) {
         width={props.symbolWidth}
       />
       {isHovered ? (
-        <div className="w-full h-full font-europa_light text-[white] bg-[rgba(0,0,0,.9)] rounded-[20px] absolute top-0 left-0 flex flex-col items-center justify-center">
-          <p className="text-2xl">{props.text}</p>
-          {props.url && (
+        <div className="w-full h-full font-europa_light text-[white] bg-[rgba(0,0,0,.9)] rounded-[20px] absolute top-0 left-0 flex flex-col items-center justify-center px-8">
+          <p className="text-2xl text-center">{props.text}</p>
+          {props.url ? (
             <button
               className="cursor-pointer w-fit h-fit text-xl px-5 py-2 mt-8 bg-[var(--green3)] rounded-full shrink-0"
               onClick={() => window.open(props.url)}
             >
               Visit Website
+            </button>
+          ) : (
+            <button
+              className="cursor-pointer w-fit h-fit text-xl px-5 py-2 mt-8 bg-[var(--green3)] rounded-full shrink-0"
+              // onClick={() => window.open(props.url)}
+            >
+              Visit Picture
             </button>
           )}
         </div>
